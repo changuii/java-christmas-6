4e046b25d69bf1c6600708253be5758ebc6a2698
changuii <rhljh7410@gmail.com>
2024-12-11 16:21:36 +0900

    feat : View를 제어할 컨트롤러를 작성하라
    - ChristmasController가 뷰와 도메인을 제어할 수 있도록 구현했다.

---
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -0,0 +1,13 @@
+package christmas.controller;
+
+import christmas.view.InputView;
+
+public class ChristmasController {
+
+    private final InputView inputView;
+
+    public ChristmasController(final InputView inputView) {
+        this.inputView = inputView;
+    }
+
+}
