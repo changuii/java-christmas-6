b1804cd387eb06f4e95687312b4eba731f6d5ed4
changuii <rhljh7410@gmail.com>
2024-12-11 20:56:41 +0900

    feat : 평일 할인을 계산하라

--- src/main/java/christmas/domain/VisitDay.java
+++ src/main/java/christmas/domain/VisitDay.java
@@ -2,6 +2,8 @@ package christmas.domain;
 
 import christmas.enums.ErrorMessage;
 import christmas.exception.CustomException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 public class VisitDay {
     private static final int VISIT_DAY_MIN = 1;
@@ -10,6 +12,9 @@ public class VisitDay {
     private static final int CHRISTMAS_D_DAY_END = 25;
     private static final int CHRISTMAS_D_DAY_DISCOUNT = 1000;
     private static final int CHRISTMAS_D_DAY_DISCOUNT_PER_DAY = 100;
+    private static final int WEEK_DAYS_DISCOUNT = 2023;
+    private static final int WEEKEND_START = 5;
+    private static final int WEEKEND_END = 6;
     private final int day;
 
     private VisitDay(final int day) {
@@ -38,8 +43,26 @@ public class VisitDay {
         return 0;
     }
 
+    public int calculateWeekDaysEvent(final int dessertCount) {
+        if (isWeekDays()) {
+            return dessertCount * WEEK_DAYS_DISCOUNT;
+        }
+        return 0;
+    }
+
     private boolean isApplicableDDay() {
         return CHRISTMAS_D_DAY_START <= day && day <= CHRISTMAS_D_DAY_END;
+    }
+
+    private boolean isWeekDays() {
+        int todayOfTheWeek = getDayOfTheWeek();
+        return todayOfTheWeek < WEEKEND_START || WEEKEND_END < todayOfTheWeek;
+    }
+
+    private int getDayOfTheWeek() {
+        return LocalDate.of(2023, 12, day)
+                .getDayOfWeek()
+                .getValue();
     }
 
     public int getVisitDay() {
