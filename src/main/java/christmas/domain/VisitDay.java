e2691cba446274973ba62ea4c10b713c8bc9d21f
changuii <rhljh7410@gmail.com>
2024-12-11 20:44:01 +0900

    feat : 크리스마스 디데이 할인을 계산하라

--- src/main/java/christmas/domain/VisitDay.java
+++ src/main/java/christmas/domain/VisitDay.java
@@ -6,6 +6,10 @@ import christmas.exception.CustomException;
 public class VisitDay {
     private static final int VISIT_DAY_MIN = 1;
     private static final int VISIT_DAY_MAX = 31;
+    private static final int CHRISTMAS_D_DAY_START = 1;
+    private static final int CHRISTMAS_D_DAY_END = 25;
+    private static final int CHRISTMAS_D_DAY_DISCOUNT = 1000;
+    private static final int CHRISTMAS_D_DAY_DISCOUNT_PER_DAY = 100;
     private final int day;
 
     private VisitDay(final int day) {
@@ -25,6 +29,17 @@ public class VisitDay {
         if (day < VISIT_DAY_MIN || VISIT_DAY_MAX < day) {
             throw new CustomException(ErrorMessage.VISIT_DAY_INPUT_INVALID);
         }
+    }
+
+    public int calculateDDayEvent() {
+        if (isApplicableDDay()) {
+            return CHRISTMAS_D_DAY_DISCOUNT + (CHRISTMAS_D_DAY_DISCOUNT_PER_DAY * (day - CHRISTMAS_D_DAY_START));
+        }
+        return 0;
+    }
+
+    private boolean isApplicableDDay() {
+        return CHRISTMAS_D_DAY_START <= day && day <= CHRISTMAS_D_DAY_END;
     }
 
     public int getVisitDay() {
