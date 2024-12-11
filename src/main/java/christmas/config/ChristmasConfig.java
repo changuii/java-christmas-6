1e2023ea079bde365691dd53d26591386e4fe7f5
changuii <rhljh7410@gmail.com>
2024-12-11 16:50:54 +0900

    feat : Controller에 OuputView 필드를 추가하고, 의존성도 주입하라

--- src/main/java/christmas/config/ChristmasConfig.java
+++ src/main/java/christmas/config/ChristmasConfig.java
@@ -4,11 +4,12 @@ import christmas.controller.ChristmasController;
 import christmas.view.InputParser;
 import christmas.view.InputValidator;
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 public abstract class ChristmasConfig {
 
     public static ChristmasController createController() {
-        return new ChristmasController(createInputView());
+        return new ChristmasController(createInputView(), new OutputView());
     }
 
     private static InputView createInputView() {
--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,13 +1,16 @@
 package christmas.controller;
 
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 public class ChristmasController {
 
     private final InputView inputView;
+    private final OutputView outputView;
 
-    public ChristmasController(final InputView inputView) {
+    public ChristmasController(final InputView inputView, final OutputView outputView) {
         this.inputView = inputView;
+        this.outputView = outputView;
     }
 
 }
